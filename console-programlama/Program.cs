f27db2fb19e6501049ba6c103171c26e1d099a9e
Ibrahim Reyhani <reyhani137reyhani@gmail.com>
2021-10-28 16:44:09 +0300

    ilk projem

---
+++ console-programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("isminizi giriniz :");
+            string ad = Console.ReadLine();
+            Console.WriteLine("soyadınızı giriniz: ");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("merhaba " + ad + " " + soyad);
+
+            Console.ReadKey();
+        }
+    }
+}
