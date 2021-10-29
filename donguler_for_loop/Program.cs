09cda1807a5f643f86f9244ff226dac1af4c73f7
Ibrahim Reyhani <reyhani137reyhani@gmail.com>
2021-10-29 17:15:20 +0300

    for döngüsü eklendi

---
+++ donguler_for_loop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen sayiya kadar olan tek sayilari yazdiriniz
+            Console.Write("sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= sayi; i++)
+            {
+                if(i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            Console.WriteLine("*****************************************");
+
+            //1 den 1000 e kadar olan tek ve çift sayilarin kendi arasindaki toplamlarini yazdiriniz
+            int tekToplam = 0, ciftToplam = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if(i % 2 == 0)
+                    ciftToplam += i;
+                if(i % 2 == 1)
+                    tekToplam += i;
+            }
+            Console.WriteLine("cift sayilarin toplami : " + ciftToplam);
+            Console.WriteLine("tek sayilarin toplami : " + tekToplam);
+
+            Console.WriteLine("*****************************************");
+
+            //break, continue
+            for (int i = 1; i < 10; i++)
+            {
+                if(i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("*****************************************");
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
