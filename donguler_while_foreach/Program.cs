5b4282f65d23e24839cbfbe5fa047d755d669404
Ibrahim Reyhani <reyhani137reyhani@gmail.com>
2021-10-29 17:34:39 +0300

    while ve foreach döngüsü eklendi

---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1 den girilen sayiya kadar olan sayilarin ortalaması
+            Console.Write("sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            float toplam = 0;
+            while (sayac <= sayi)
+            {
+                 toplam += sayac;
+                 sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            // 'a' dan 'z' ye kadar olan tüm harfleri ekrana yazdırın
+            char character = 'a';
+            while (character <= 'z')
+            {
+                 Console.Write($"{character,3}");
+                 character++;
+            }
+
+            Console.WriteLine();
+
+            //foreach
+            string[] arabalar = {"Ford", "Mazda", "Toyota"};
+
+            foreach (var a in arabalar)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
