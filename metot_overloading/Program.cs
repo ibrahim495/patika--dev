25d3930544faa67976c6b7323a39624e57c2dcf4
Ibrahim Reyhani <reyhani137reyhani@gmail.com>
2021-10-30 14:57:19 +0300

    metot overloading eklendi

---
+++ metot_overloading/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace metot_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Out parametreler
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2, 3, out int toplam);
+            Console.WriteLine(toplam);
+
+            //Metot Aşırı Yükleme
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade); // artık buna hata vermiyor
+
+            //Metot imzası
+            //Metot adi + parametre sayisi + parametre
+
+            instance.EkranaYazdir("İbrahim", "Reyhani");
+
+            Console.ReadKey();
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + " " + veri2);
+        }
+    }
+}
